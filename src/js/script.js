fb296fc0df979f34ab3bfcc94ef9a4cb401c6b85
annsinkevich47 <anneta.sinkevich@mail.ru>
2023-10-15 21:08:29 +0300

    add slides

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-alert(1)
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png" alt="arrow1"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png" alt="arrow2"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
